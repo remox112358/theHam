2b136314a0cc1457e59ff2e6bac7992b15c6a087
remox112358 <remox112358@gmail.com>
2021-01-30 15:54:48 +0400

    Add: about section

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -26,3 +26,71 @@ window.lazyLoad = () => {
 window.onload = () => {
   window.lazyLoad()
 }
+
+jQuery(($) => {
+
+  $(document).ready(() => {
+
+    skillsEffect();
+
+    $('#tabs-about .tab').click(function(event) {
+      if (! $(this).hasClass('active')) {
+        $('#tabs-about .tab').removeClass('active');
+        $(this).addClass('active');
+
+        $('.about .area').removeClass('active');
+        $(`${$(this).attr('data-href')}`).addClass('active');
+      }
+
+      event.preventDefault();
+    })
+
+  })
+
+
+  /**
+   * Starting skill bars effect.
+   */
+  const skillsEffect = (transition = 2) => {
+
+    var bars = [
+      {
+        element: $('#skill-ui .skill__bar'),
+        width: 75,
+        color: '#9c5da5'
+      },
+      {
+        element: $('#skill-wd .skill__bar'),
+        width: 85,
+        color: '#11b0de'
+      },
+      {
+        element: $('#skill-wp .skill__bar'),
+        width: 70,
+        color: '#d67f7f'
+      },
+      {
+        element: $('#skill-hc .skill__bar'),
+        width: 90,
+        color: '#20bc9d'
+      },
+      {
+        element: $('#skill-ad .skill__bar'),
+        width: 85,
+        color: '#bb8a36'
+      },
+    ];
+
+    bars.forEach(bar => {
+
+      bar.element.css({
+        transition: String(transition) + 's',
+        width: String(bar.width) + '%',
+        background: bar.color
+      });
+
+    });
+
+  }
+
+})
