a25a3b71cfdfe61329c760465740768db813dd1b
remox112358 <remox112358@gmail.com>
2021-02-04 17:37:23 +0400

    Add footer

--- src/assets/js/yandexMap.js
+++ src/assets/js/yandexMap.js
@@ -3,7 +3,7 @@ var map;
 ymaps.ready(() => {
 
   map = new ymaps.Map('map', {
-    center: [55.76, 37.64],
+    center: [34.052235, -118.243683],
     zoom: 10
   }, {
     searchControlProvider: 'yandex#search'
